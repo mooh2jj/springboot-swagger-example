33d881c8a0525a425b2c54ca1e217f7fc063a20f
seonggon do <62453668+mooh2jj@users.noreply.github.com>
2022-03-16 21:35:47 +0900

    swagger config 설정 가능

---
+++ src/test/java/com/dsg/springbootswaggerexample/SpringbootSwaggerExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsg.springbootswaggerexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSwaggerExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
